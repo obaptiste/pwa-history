217e289e5a46c3b7f13381ad88ca5631f155f32a
Oris John-Baptiste <orisjb@gmail.com>
2022-06-14 22:54:42 +0100

    initial commit

---
+++ src/About.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About: React.FC = () => (
+  <h1>This is a PWA</h1>
+);
+
+export default About;
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders about link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/about/i);
   expect(linkElement).toBeInTheDocument();
 });
+
